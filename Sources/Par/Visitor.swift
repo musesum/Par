b4b7a0d2c9d4559f07b8b9869a099ccaf3014459
musesum <musesum@gmail.com>
2022-12-21 19:44:22 +0800

    synchronize menu peers

--- Sources/Par/Visitor.swift
+++ Sources/Par/Visitor.swift
@@ -14,8 +14,23 @@ public class Visitor {
     
     public var visited = Set<Int>()
 
+    public init () {
+    }
     public init (_ id: Int) {
         visited.insert(id)
+    }
+    public func fromRemote() -> Visitor {
+        visited.insert("remote".hash)
+        return self
+    }
+    public func wasRemote() -> Bool {
+        return visited.contains("remote".hash)
+    }
+    
+    public func startVisit(_ id: Int,_ visit: @escaping ()->()) {
+        if visited.contains(id) { return }
+        visited.insert(id)
+        visit()
     }
 
     public func newVisit(_ id: Int) -> Bool {
