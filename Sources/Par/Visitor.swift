c99d474388a2a9701b9005eab2fff86cf06c13f5
musesum <musesum@gmail.com>
2023-01-22 09:51:36 +0800

    Refresh Visitor enums

--- Sources/Par/Visitor.swift
+++ Sources/Par/Visitor.swift
@@ -3,28 +3,54 @@
 
 import Foundation
 
+public struct VisitFrom: OptionSet {
+
+    public let rawValue: Int
+
+    public static let model   = VisitFrom(rawValue: 1 << 0) ///  1
+    public static let user    = VisitFrom(rawValue: 1 << 1) ///  2
+    public static let remote  = VisitFrom(rawValue: 1 << 2) ///  4
+    public init(rawValue: Int = 0) { self.rawValue = rawValue }
+
+    static public var debugDescriptions: [(Self, String)] = [
+        (.model  , "model"  ),
+        (.user  , "user"  ),
+        (.remote , "remote" ),
+    ]
+
+    public var description: String {
+        let result: [String] = Self.debugDescriptions.filter { contains($0.0) }.map { $0.1 }
+        let joined = result.joined(separator: ", ")
+        return "\(joined)"
+    }
+    public var remote: Bool { self.contains(.remote) }
+    public var user: Bool { self.contains(.user) }
+    public var model: Bool { self.contains(.model) }
+}
+
 /// Visit a node only once. Collect and compare with a set of nodes already visited.
 public class Visitor {
 
     static var Id = 0  // unique identifier for each node
     public static func nextId() -> Int { Id += 1; return Id }
-    static let FromRemote = "__VisitorFromRemote__".hash
 
     private var lock = NSLock()
     private var visited = Set<Int>()
 
-    public init (_ id: Int) {
+    public var from: VisitFrom
+
+    public init (_ id: Int = 0) {
+        from = .model
         nowHere(id)
     }
-    public init (fromRemote: Bool = false) {
-        if fromRemote {
-            nowHere(Visitor.FromRemote)
-        }
+    public init (_ from: VisitFrom) {
+        self.from = from
     }
-    public func wasRemote() -> Bool {
-        return wasHere(Visitor.FromRemote)
-    }
-    private func nowHere(_ id: Int) {
+    public var fromRemote: Bool { from.contains(.remote) }
+    public var fromUser: Bool { from.contains(.user) }
+    public var fromModel: Bool { from.contains(.model) }
+
+    public func nowHere(_ id: Int) {
         lock.lock()
         visited.insert(id)
         lock.unlock()
@@ -36,7 +62,7 @@ public class Visitor {
         return contains
     }
     public func isLocal() -> Bool {
-        return !wasRemote()
+        return !from.contains(.remote)
     }
 
     public func newVisit(_ id: Int) -> Bool {
